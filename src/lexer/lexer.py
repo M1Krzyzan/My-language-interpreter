9e3f1e936284a2e4016029e1ea22837b02fa54e5
Krzyzan <michal.13777@gmail.com>
2025-04-02 13:04:40 +0200

    added missing changes

--- src/lexer/lexer.py
+++ src/lexer/lexer.py
@@ -1,24 +1,72 @@
 from src.lexer.source.source import Source
 from src.lexer.source.string import StringSource
+from src.lexer.token_ import TokenType,Token,Symbols
+
+BEGIN_COMMENT = '#'
+
 
 class Lexer:
     def __init__(self, source: Source):
         self.source = source
-        self.column = 0
-        self.row = 0
+        self.current_char = self.source.next()
+        self.column = 1
+        self.row = 1
 
-    def get_token(self):
-        while not self.source.eof():
-            char = self.source.peek()
-            if char.isspace():
-                self.source.next()
-                continue  # Skip whitespace
+    def next_token(self):
+        self._skip_white_characters_and_comments()
 
-            return self.source.next()  # Return the next meaningful character
+        if self.current_char == '\uFFFF':
+            return Token(TokenType.EOF, (self.column, self.row), None)
 
-        return None
+        return (self._process_keyword_or_identifier()
+                or self._process_string_literal()
+                or self._process_boolean_literal()
+                or self._process_number_literal()
+                or self._special_character())
+
+    def _skip_white_characters_and_comments(self):
+        while self.current_char.isspace() or self.current_char == BEGIN_COMMENT:
+            if self.current_char == BEGIN_COMMENT:
+                while self.current_char != '\n':
+                    self.current_char = self.source.next()
+                    self.column += 1
+            self.current_char = self.source.next()
+            if self.current_char != '\n':
+                self.column = 1
+                self.row += 1
+
+    def _process_keyword_or_identifier(self):
+        if not self.current_char.isalpha() and self.current_char != '_':
+            return None # I don't know whether I should raise error here or just return None
+
+        name = []
+        while self.current_char.isalpha() or self.current_char == "_" or self.current_char.isdigit():
+            name.append(self.current_char)
+            self.current_char = self.source.next()
+            self.column += 1
+        name = "".join(name)
+
+        return Token(Symbols.keywords.get(name, TokenType.IDENTIFIER),(self.column, self.row), name)
+
+    def _process_string_literal(self):
+        pass
+
+    def _process_boolean_literal(self):
+        pass
+
+    def _process_number_literal(self):
+        pass
+
+    def _special_character(self):
+        pass
 
 
 if __name__ == "__main__":
-    code_source = StringSource("hello world")
+    code =(
+"""#COMMENT
+int sum(float x, int y){ #COMMENT 
+  return x to int + y;
+}""")
+    code_source = StringSource(code)
     lexer = Lexer(code_source)
+    lexer.next_token()
--- src/lexer/token_.py
+++ src/lexer/token_.py
@@ -1,19 +1,22 @@
 from enum import Enum, auto
+from typing import Tuple, Optional
+
 
 class TokenType(Enum):
-
     IF_KEYWORD = auto()
+    ELIF_KEYWORD = auto()
     ELSE_KEYWORD = auto()
     WHILE_KEYWORD = auto()
-    FOREACH_KEYWORD = auto()
     RETURN_KEYWORD = auto()
-    AS_KEYWORD = auto()
+    BREAK_KEYWORD = auto()
+    CONTINUE_KEYWORD = auto()
+    TO_KEYWORD = auto()
     VOID_KEYWORD = auto()
     INT_KEYWORD = auto()
     FLOAT_KEYWORD = auto()
     STRING_KEYWORD = auto()
-    DICT_KEYWORD = auto()
-    NULL_KEYWORD = auto()
+    BOOL_KEYWORD = auto()
+    EXCEPTION_KEYWORD = auto()
 
     LEFT_ROUND_BRACKET = auto()
     RIGHT_ROUND_BRACKET = auto()
@@ -32,7 +35,6 @@ class TokenType(Enum):
     DIVISION_OPERATOR = auto()
     MODULO_OPERATOR = auto()
     NEGATION_OPERATOR = auto()
-    NULLABLE_OPERATOR = auto()
 
     OR_OPERATOR = auto()
     AND_OPERATOR = auto()
@@ -48,16 +50,64 @@ class TokenType(Enum):
     STRING_LITERAL = auto()
     INT_LITERAL = auto()
     FLOAT_LITERAL = auto()
+    BOOL_LITERAL = auto()
     EOF = auto()
 
 
+class Symbols:
+    keywords = {
+        "if": TokenType.IF_KEYWORD,
+        "elif": TokenType.ELIF_KEYWORD,
+        "else": TokenType.ELSE_KEYWORD,
+        "while": TokenType.WHILE_KEYWORD,
+        "return": TokenType.RETURN_KEYWORD,
+        "break": TokenType.BREAK_KEYWORD,
+        "continue": TokenType.CONTINUE_KEYWORD,
+        "to": TokenType.TO_KEYWORD,
+        "void": TokenType.VOID_KEYWORD,
+        "int": TokenType.INT_KEYWORD,
+        "float": TokenType.FLOAT_KEYWORD,
+        "string": TokenType.STRING_KEYWORD,
+        "bool": TokenType.BOOL_KEYWORD,
+        "Exception": TokenType.EXCEPTION_KEYWORD,
+        "or": TokenType.OR_OPERATOR,
+        "and": TokenType.AND_OPERATOR,
+        "not": TokenType.NEGATION_OPERATOR
+    }
+
+    single_char = {
+        "(": TokenType.LEFT_ROUND_BRACKET,
+        ")": TokenType.RIGHT_ROUND_BRACKET,
+        "[": TokenType.LEFT_SQUARE_BRACKET,
+        "]": TokenType.RIGHT_SQUARE_BRACKET,
+        "{": TokenType.LEFT_CURLY_BRACKET,
+        "}": TokenType.RIGHT_CURLY_BRACKET,
+        ",": TokenType.COMMA,
+        ":": TokenType.COLON,
+        ";": TokenType.SEMICOLON,
+        "+": TokenType.PLUS_OPERATOR,
+        "-": TokenType.MINUS_OPERATOR,
+        "*": TokenType.MULTIPLICATION_OPERATOR,
+        "/": TokenType.DIVISION_OPERATOR,
+        "%": TokenType.MODULO_OPERATOR,
+        "<": TokenType.LESS_THAN_OPERATOR,
+        ">": TokenType.GREATER_THAN_OPERATOR,
+        "=": TokenType.ASSIGNMENT,
+    }
+
+    double_char = {
+        "<=": TokenType.LESS_THAN_OR_EQUAL_OPERATOR,
+        ">=": TokenType.GREATER_THAN_OR_EQUAL_OPERATOR,
+        "==": TokenType.EQUAL_OPERATOR,
+        "!=": TokenType.NOT_EQUAL_OPERATOR
+    }
+
+
 class Token:
-    def __init__(self, type_: TokenType, value: str = "", position: int = 0):
+    def __init__(self, type_: TokenType, position: Tuple = (), value: Optional = None):
         self.type = type_
         self.value = value
         self.position = position  # Can help with debugging
 
     def __repr__(self):
         return f"Token({self.type}, {repr(self.value)}, pos={self.position})"
-
-
